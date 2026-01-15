6975ed969a2f0e1b9edfc3df86b3d6d1f618d171
debaprya banerjee <banerjee.debapriya9@gmail.com>
2026-01-15 12:03:29 +0100

    Added ACR creation logic

--- Terraform_Cluster/main.tf
+++ Terraform_Cluster/main.tf
@@ -31,3 +31,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
+
+# 5. Create the Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "myuniqueacrname123" # Must be globally unique
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Basic" # Cheapest option for free tier/testing
+  admin_enabled       = false
+}
+
+# 6. ATTACH ACR TO AKS (The "AcrPull" Role)
+resource "azurerm_role_assignment" "aks_to_acr" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
